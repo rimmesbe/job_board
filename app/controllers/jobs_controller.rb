e1d660db5422861fbb2ceaeb6d4a4659c65834b7
rimmesbe <ryanimmesberger@gmail.com>
2015-09-28 18:34:55 -0400

    added update route

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,6 +17,12 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+  def update
+    @job = Job.find(params[:id])
+    @job.update_attributes(job_params)
+    redirect_to jobs_path
+  end
+
   private
 
   def job_params

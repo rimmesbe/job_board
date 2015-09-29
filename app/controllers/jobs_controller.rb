34a03bad6d040e17cefc830763c22fe4d3c21242
rimmesbe <ryanimmesberger@gmail.com>
2015-09-29 16:15:23 -0400

    added flash notices on job edit,update,destroy routes

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -23,19 +23,28 @@ class JobsController < ApplicationController
 
   def edit
     @job = Job.find(params[:id])
-    return redirect_to job_path(@job) unless current_user.is_owner?(@job)
+    unless current_user.is_owner?(@job)
+      flash[:notice] = "You don't own this post"
+      return redirect_to job_path(@job)
+    end
   end
 
   def update
     @job = Job.find(params[:id])
-    return redirect_to job_path(@job) unless current_user.is_owner?(@job)
+    unless current_user.is_owner?(@job)
+      flash[:notice] = "You don't own this post"
+      return redirect_to job_path(@job)
+    end
     @job.update_attributes(job_params)
     redirect_to jobs_path
   end
 
   def destroy
     @job = Job.find(params[:id])
-    return redirect_to job_path(@job) unless current_user.is_owner?(@job)
+    unless current_user.is_owner?(@job)
+      flash[:notice] = "You don't own this post"
+      return redirect_to job_path(@job)
+    end
 
     @job.destroy
     redirect_to jobs_path

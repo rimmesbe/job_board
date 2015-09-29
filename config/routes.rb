6b28386098aa1c5924372f67208ef7c3a160a717
rimmesbe <ryanimmesberger@gmail.com>
2015-09-29 15:00:10 -0400

    implemented devise user administration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'jobs#index'
-  devise_for :users
+  root to: 'jobs#index'
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   resources :jobs
 
   # The priority is based upon order of creation: first created -> highest priority.

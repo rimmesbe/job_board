c206e62e647c55560a625911ecd6c385904a3449
rimmesbe <ryanimmesberger@gmail.com>
2015-09-30 15:47:03 -0400

    added some user + job tests, created some fixtures

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid with all attributes" do
+    j = jobs(:carpenter)
+    assert j.valid?, "Job is not valid"
+  end
+
+  test "invalid with no title" do
+    j = Job.new
+    assert !j.valid?, "Title not being validated"
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def test_invalid_without_name
+  def setup
+    @u = users(:bob)
+  end
+
+  test "invalid without a name" do
     user = User.new
     assert !user.valid?, "Name is not being validated"
+  end
+
+  test "valid with all attributes" do
+    assert @u.valid?, "User is not valid"
+  end
+
+  test "should respond to jobs" do
+    assert_respond_to @u, :jobs
+  end
+
+  test "is_owner? returns true if user owns job" do
+    assert_equal true, @u.is_owner?(jobs(:carpenter))
+  end
+
+  test "is_owner? returns false if user doesn't own job" do
+    assert_equal false, @u.is_owner?(jobs(:doctor))
+  end
+
+  test "should contain only jobs that belong to user" do
+    assert @u.jobs.all? {|j| j.user == @u }
   end
 end

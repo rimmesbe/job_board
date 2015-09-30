36576eda8d8d7a1b85aca270cfbe30b6dd73d606
rimmesbe <ryanimmesberger@gmail.com>
2015-09-30 16:21:38 -0400

    caught up job tests

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -1,14 +1,35 @@
 require 'test_helper'
 
 class JobTest < ActiveSupport::TestCase
-  test "valid with all attributes" do
-    j = jobs(:carpenter)
-    assert j.valid?, "Job is not valid"
+  def setup
+    @j = jobs(:carpenter)
   end
 
   test "invalid with no title" do
-    j = Job.new
-    assert !j.valid?, "Title not being validated"
+    job = Job.new
+    assert !job.valid?, "Title not being validated"
+  end
+
+  test "invalid title gives error message" do
+    @j.title = nil
+    assert_presence(@j, :title)
+  end
+
+  test "invalid description gives error message" do
+    @j.description = nil
+    assert_presence(@j, :description)
+  end
+
+  test "valid with all attributes" do
+    assert @j.valid?, "Job is not valid"
+  end
+
+  test "should respond to user" do
+    assert_respond_to @j, :user
+  end
+
+  test "should only have 1 user" do
+    assert_equal users(:bob), @j.user
   end
 
 end

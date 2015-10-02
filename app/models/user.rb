7d720bafba5af56da62216df82e56997f3cd647e
rimmesbe <ryanimmesberger@gmail.com>
2015-10-01 22:12:18 -0400

    added dependent: :destroy on user to jobs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :jobs
+  has_many :jobs, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
